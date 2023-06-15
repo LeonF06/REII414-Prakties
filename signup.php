f74526418f2cb669a8811ecb2b05f8a2eb022867
LeonF06 <75602498+LeonF06@users.noreply.github.com>
2023-06-15 15:05:16 +0200

    Sign_up_page_v1.0
    UI elements is op

--- signup.php
+++ signup.php
@@ -1,11 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Campus Cribs - Sign up</title>
+    <title>Sign Up</title>
+    <script>
+        function showFields() {
+            var role = document.querySelector('input[name="role"]:checked').value;
+            var fieldsContainer = document.getElementById("fieldsContainer");
+            if (role === 'student') {
+                fieldsContainer.innerHTML = `
+                    <label for="firstName">First Name:</label>
+                    <input type="text" name="firstName" id="firstName">
+                    <br>
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" name="lastName" id="lastName">
+                    <br>
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email">
+                    <br>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                `;
+            } else if (role === 'landlord') {
+                fieldsContainer.innerHTML = `
+                    <label for="firstName">First Name:</label>
+                    <input type="text" name="firstName" id="firstName">
+                    <br>
+                    <label for="lastName">Last Name:</label>
+                    <input type="text" name="lastName" id="lastName">
+                    <br>
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email">
+                    <br>
+                    <label for="cell">Cell:</label>
+                    <input type="text" name="cell" id="cell">
+                    <br>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                    <br>
+                    <label for="photo">Photo:</label>
+                    <input type="file" name="photo" id="photo" accept="image/*" onchange="previewPhoto(event)">
+                    <br>
+                    <img id="photoPreview" src="#" alt="Photo Preview" style="max-width: 200px; max-height: 200px;">
+                `;
+            } else {
+                fieldsContainer.innerHTML = '';
+            }
+        }
+        
+        function previewPhoto(event) {
+            var photoInput = event.target;
+            var photoPreview = document.getElementById('photoPreview');
+            
+            if (photoInput.files && photoInput.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    photoPreview.src = e.target.result;
+                };
+                reader.readAsDataURL(photoInput.files[0]);
+            } else {
+                photoPreview.src = '#';
+            }
+        }
+        
+        function goBack() {
+            window.history.back();
+        }
+    </script>
 </head>
 <body>
-  <h1>Sign up</h1>
-  <p>This is the sign up page page.</p>
-  <!-- Add your sign up content here -->
+    <h1>Sign Up</h1>
+
+    <form method="POST" action="signup_process.php" enctype="multipart/form-data">
+        <label>Choose your role:</label>
+        <br>
+        <input type="radio" name="role" value="student" id="student" onclick="showFields()">
+        <label for="student">Student</label>
+        <br>
+        <input type="radio" name="role" value="landlord" id="landlord" onclick="showFields()">
+        <label for="landlord">Landlord</label>
+        <br>
+        <br>
+        <div id="fieldsContainer"></div>
+        <br>
+        <input type="submit" value="Sign Up">
+        <button type="button" onclick="goBack()">Back</button>
+    </form>
 </body>
 </html>
