17f1817bd8d8cd81e1f567eeba96419fa87649f6
Gerrit Coetzer <75617224+GertLC@users.noreply.github.com>
2023-06-16 00:55:09 +0200

    Updated database page to show properties and their corresponding photos

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
 </head>
 <body>
   <h1>Databases</h1>
-  <h1>Students</h1>
+  <h1>Students:</h1>
   <!-- Add your database-related content here -->
 
   <?php
@@ -23,7 +23,7 @@
     }
   ?>
   
-  <h1>Landlords</h1>
+  <h1>Landlords:</h1>
 
   <?php
     $sql = "SELECT Land_ID, FName, LName, Land_Email, Land_Pass, Cell_Number, Land_Phot FROM Landlords";
@@ -46,8 +46,43 @@
     } else {
       echo "0 results";
     }
-    $mysqli->close();
   ?>
+
+<h1>Properties:</h1>
+
+<?php
+  $sql = "SELECT Prop_ID, Prop_Address, Price, Avail, Popul, DOL FROM Properties";
+  $result = $mysqli->query($sql);
+
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          $propID = $row["Prop_ID"];
+          echo " Property ID: " . $propID . " Address: " . $row["Prop_Address"] . " Price: " . $row["Price"] . " Availability: " . $row["Avail"] . " Popularity: " . $row["Popul"] .  " Date of Listing: " . $row["DOL"] . " Property Photos: " . "<br>";
+
+          // Retrieve related photos
+          $photosSql = "SELECT Phot_Data FROM Photos WHERE Prop_ID = '$propID'";
+          $photosResult = $mysqli->query($photosSql);
+
+          if ($photosResult->num_rows > 0) {
+              while ($photoRow = $photosResult->fetch_assoc()) {
+                  $photoData = $photoRow["Phot_Data"];
+                  $photoSrc = 'data:image/jpeg;base64,' . base64_encode($photoData);
+                  echo "<div style='display: inline-block; margin-right: 10px;'>";
+                  echo "<img src='$photoSrc' alt='Property Photo' style='max-width: 200px; max-height: 200px;'>";
+                  echo "</div>";
+              }
+          } else {
+              echo "No photos available<br>";
+          }
+
+          echo "<br>";
+      }
+  } else {
+      echo "0 results";
+  }
+  $mysqli->close();
+?>
 
 </body>
 </html>
